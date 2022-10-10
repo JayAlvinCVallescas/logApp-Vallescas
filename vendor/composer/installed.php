50f2fa58979b421a72280023a1c337dfc9559212
Jay Alvin C. Vallescas <202080209@psu.palawan.edu.ph>
2022-10-10 21:06:58 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jayalvincvallescas/logapp-vallescas',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '88e7f3890b4f7f5830f89b35d43a65a5d89b5790',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jayalvincvallescas/logapp-vallescas' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '88e7f3890b4f7f5830f89b35d43a65a5d89b5790',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
